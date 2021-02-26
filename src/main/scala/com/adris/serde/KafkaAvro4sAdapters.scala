bc4f56eebf0e4afdd0112f58abbf642249f83305
Adrian Salajan <adrian.salajan@ing.com>
2021-02-26 17:06:36 +0200

    code changes

---
+++ src/main/scala/com/adris/serde/KafkaAvro4sAdapters.scala
@@ -0,0 +1,58 @@
+package com.adris.serde
+
+import com.sksamuel.avro4s.{ Decoder, Encoder, FromRecord, ToRecord }
+import io.confluent.kafka.serializers.{ KafkaAvroDeserializer, KafkaAvroSerializer }
+import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.common.serialization.{ Deserializer, Serializer }
+
+import java.util
+
+object KafkaAvro4sAdapters {
+
+  implicit class EncoderAdapter[A](encoder: Encoder[A]) {
+
+    def toKafkaAvroSerializer: Serializer[A] =
+      new Serializer[A] {
+
+        private val kafkaAvroSerializer = {
+          val ser = new KafkaAvroSerializer
+          ser.asInstanceOf[Serializer[GenericRecord]]
+        }
+
+        override def configure(
+          configs: util.Map[String, _],
+          isKey: Boolean
+        ): Unit =
+          kafkaAvroSerializer.configure(configs, isKey)
+
+        override def serialize(topic: String, data: A): Array[Byte] = {
+          implicit val enc = encoder
+          kafkaAvroSerializer.serialize(topic, ToRecord[A].to(data))
+        }
+      }
+  }
+
+  implicit class DecoderAdapter[A](decoder: Decoder[A]) {
+
+    def toKafkaAvroDeserializer: Deserializer[A] =
+      new Deserializer[A] {
+
+        private val kafkaAvroDeserializer = {
+          val deser = new KafkaAvroDeserializer
+//          deser.configure(config.asJava, isKey)
+          deser.asInstanceOf[Deserializer[GenericRecord]]
+        }
+
+        override def configure(
+          configs: util.Map[String, _],
+          isKey: Boolean
+        ): Unit =
+          kafkaAvroDeserializer.configure(configs, isKey)
+
+        override def deserialize(topic: String, data: Array[Byte]): A = {
+          implicit val enc = decoder
+          FromRecord[A].from(kafkaAvroDeserializer.deserialize(topic, data))
+        }
+      }
+  }
+}
